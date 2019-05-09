6d19a0cdc87be138919360c1a6b4e3b805a157b9
Oussama Benkejjane <oussama.benkejjane@gmail.com>
2019-05-09 17:47:03 +0000

    Fix category component

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -5,6 +5,7 @@
     <link href="{{URL::to('dist/css/style.min.css')}}" rel="stylesheet">
 @endsection
 
+
 @section('content')
     <category :response="{{ json_encode($categories) }}"></category>
 @endsection
